152d2bc5396505727704e1d488aff60933fea214
Yogesh Kumar <yogeshgopal98@gmail.com>
2025-01-08 18:50:50 -0500

    node check api

--- app.js
+++ app.js
@@ -1,6 +1,8 @@
 // app.js
 
 const express = require('express');
+const os = require('os'); // Import os module
+const fs = require('fs'); // Import fs module to read from the file system
 const app = express();
 const port = process.env.PORT || 3000;
 
@@ -12,6 +14,21 @@ app.get('/', (req, res) => {
 // Sample API endpoint
 app.get('/api', (req, res) => {
   res.json({ message: 'Welcome to the API!' });
+});
+
+// New endpoint to get pod and node information
+app.get('/node', (req, res) => {
+  // Get the hostname (pod name)
+  const hostname = os.hostname();
+
+  // Read node name from the Kubernetes downward API (we'll set this up later)
+  const nodeName = process.env.NODE_NAME || 'Unknown';
+
+  res.json({
+    message: 'Hello from Kubernetes!',
+    pod: hostname,
+    node: nodeName,
+  });
 });
 
 // Start the server
